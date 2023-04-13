0c8fbd38ebf3b013313ad77bf46d71b4a4f6d326
d4rm <dantemartinez06@gmail.com>
2023-04-13 17:59:32 -0300

    commit test

---
+++ script.js
@@ -0,0 +1,28 @@
+// DOMS consts
+const calcScreen = document.querySelector('.calc__screen');
+// Delete consts
+const backspaceButton = document.querySelector('.btn__delete--backspace');
+const ceButton = document.querySelector('.btn__delete--ce');
+const cButton = document.querySelector('.btn__delete--c');
+// Number consts
+const sevenButton = document.querySelector('.btn__number--seven');
+const eightButton = document.querySelector('.btn__number--eight');
+const nineButton = document.querySelector('.btn__number--nine');
+const fourButton = document.querySelector('.btn__number--four');
+const fiveButton = document.querySelector('.btn__number--five');
+const sixButton = document.querySelector('.btn__number--six');
+const oneButton = document.querySelector('.btn__number--one');
+const twoButton = document.querySelector('.btn__number--two');
+const threeButton = document.querySelector('.btn__number--three');
+const zeroButton = document.querySelector('.btn__number--zero');
+// Operation consts
+const positivityButton = document.querySelector('.btn__special--positivity');
+const decimalButton = document.querySelector('.btn__special--decimal');
+const additionButton = document.querySelector('.btn__special--addition');
+const subtractButton = document.querySelector('.btn__special--subtract');
+const equalsButton = document.querySelector('.btn__special--equals');
+const fractionButton = document.querySelector('.btn__special--fraction');
+const percentageButton = document.querySelector('.btn__special--percentage');
+const sqrtButton = document.querySelector('.btn__special--sqrt');
+const divisionButton = document.querySelector('.btn__special--division');
+const multiplyButton = document.querySelector('.btn__special--multiply');
